84c539dce98d5f8e4095bd9dc41a5949510b73c1
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-04-25 22:43:51 +0100

    Added movement to enemy and raycast to detect game objects

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,8 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject enemyRayCastOrigin;
+    public Rigidbody2D rigidbody2D;
+    private float speed = 100f;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        DrawEnemyRayCast();
+        EnemyMovement();
+    }
+
+    private void DrawEnemyRayCast()
+    {
+        // Create a raycast and check what it hits
+        float rayCastLength = 1f;
+        Debug.DrawRay(enemyRayCastOrigin.transform.position, 
+            transform.TransformDirection(Vector2.right) * rayCastLength, Color.magenta);
+
+        RaycastHit2D hit = Physics2D.Raycast(enemyRayCastOrigin.transform.position,
+            transform.TransformDirection(Vector2.right), rayCastLength);
+        
+
+        if (hit)
+        {
+            Debug.Log(gameObject.name + "Enemy hit " + hit.collider.name);
+        }
+    }
+
+    private void EnemyMovement()
+    {
+        rigidbody2D.AddRelativeForce(Vector2.right * speed * Time.deltaTime);
+    }
+
+
 }
